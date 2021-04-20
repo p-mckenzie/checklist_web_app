50ec45d0af5cb1abc3c0b5faba09257add6693fe
Paige McKenzie <paige.mckenzie95@yahoo.com>
2021-04-19 21:18:40 -0400

    update sample data

--- sampledata.sql
+++ sampledata.sql
@@ -2,11 +2,11 @@ DELETE FROM users WHERE username='test';
 DELETE FROM tasks WHERE user_id=1;	
 INSERT INTO users (username,hash,id) values ('test',116,1);
 INSERT INTO tasks (user_id,title,date,freq) values 
-		(1,'Neg',date('now','-1 days'),1),
-		(1,'Now',date('now'),0),
-		(1,'Foo',date('now','+1 days'),2),
-		(1,'Bar',date('now','+2 days'),3),
-		(1,'Baz',date('now','+3 days'),4),
-		(1,'Qux',date('now','+4 days'),1),
-		(1,'Quux',date('now','+5 days'),1),
-		(1,'Later',date('now','+12 days'),1);
+		(1,'Laundry',date('now','-1 days'),1),
+		(1,'Run dishwasher',date('now'),0),
+		(1,'Take cat to vet',date('now','+1 days'),2),
+		(1,'Take out the trash',date('now','+2 days'),3),
+		(1,'Get haircut',date('now','+3 days'),4),
+		(1,'Grocery shop',date('now','+4 days'),1),
+		(1,'Clean bathroom',date('now','+5 days'),1),
+		(1,'Organize bookshelf',date('now','+12 days'),1);
